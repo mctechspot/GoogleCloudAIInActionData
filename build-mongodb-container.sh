0a91e9663e1db92c96ee8222f1b682585b0822f9
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2025-06-13 12:13:51 +0200

    add build-mongodb-container bash script file

--- build-mongodb-container.sh
+++ build-mongodb-container.sh
@@ -5,13 +5,22 @@ set -o allexport
 source .env
 set +o allexport
 
-# Set variables for container
-CONTAINER_NAME=mongodb-container
-IMAGE_NAME=mongodb/mongodb-community-server
+# Set variables for docker image and container
+IMAGE=mongodb/mongodb-community-server
 IMAGE_TAG=latest
+CONTAINER_NAME=mongodb-container
+
+# Check and pull for MongoDB community image if it does not exist
+IMAGE_LISTED=$(docker image ls | grep $IMAGE)
+if [ -z "$IMAGE_LISTED" ]; then
+    echo Image $IMAGE does not exist on device. Pulling now...
+    docker pull $IMAGE:$IMAGE_TAG
+else
+    echo Image $IMAGE already exists on device
+fi
 
 # Build container
 docker run --name $CONTAINER_NAME -p $DB_PORT:27017 \
 -e MONGODB_INITDB_ROOT_USERNAME=$DB_USER \
 -e MONGODB_INITDB_ROOT_PASSWORD=$DB_PASSWORD \
--d $IMAGE_NAME:$IMAGE_TAG
+-d $IMAGE:$IMAGE_TAG
